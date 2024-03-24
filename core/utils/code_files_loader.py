bfadf028a6a5d6a939c41d3e8901d2babb15619a
Nikolas Papadopoulos <53314070+NikolasPpd@users.noreply.github.com>
2024-03-24 20:02:19 +0200

    Fix code_file_loader comments

--- core/utils/code_files_loader.py
+++ core/utils/code_files_loader.py
@@ -4,13 +4,13 @@ from .code_file import CodeFile
 
 def read_files_from_directory(directory: str):
     """
-    Read the contents of all .java files in the specified directory.
+    Read the contents of all .java files in the specified directory. The author and timestamp fields are left empty.
 
     Parameters:
         directory (str): The directory containing the .java files.
 
     Returns:
-        dict: A dictionary with filenames as keys and their JavaFile objects as values.
+        dict: A dictionary with filenames as keys and their CodeFile objects as values.
     """
     files = [f for f in os.listdir(directory) if f.endswith(".java")]
     contents = {}
@@ -25,13 +25,13 @@ def read_files_from_directory(directory: str):
 
 def read_files_from_dict_list(dict_list: list):
     """
-    Read the contents of all .java files in the specified directory.
+    Read the contents of all .java files in the directories found inside the git contribution dictionaries.
 
     Parameters:
         dict_list (list): A list of git contribution dictionaries.
 
     Returns:
-        dict: A dictionary with filenames as keys and their JavaFile objects as values.
+        dict: A dictionary with filenames as keys and their CodeFile objects as values.
     """
     contents = {}
 
